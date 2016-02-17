d837dfbe60daa7936f34840675ca5601da6b15c5
gkardava <qardava.giorgi@gmail.com>
2016-02-17 16:59:37 +0400

    remove and change secrets

--- KeePass/Utils/ApiKeys.cs
+++ KeePass/Utils/ApiKeys.cs
@@ -2,14 +2,17 @@
 
 namespace KeePass.Utils
 {
-    internal static class ApiKeys
+
+#warning remove git_mode :)
+#if git_mode
+     internal static partial class ApiKeys
     {
 #warning WinPass needs API Keys to use web services
-        public const string DROPBOX_KEY = "94lo7zth31vbtxl";
-        public const string DROPBOX_SECRET = "aeczxjl4dam8nl9";
-        public const string MIXPANEL_TOKEN = "YOUR_MIXPANEL_TOKEN";
-        public const string ONEDRIVE_SECRET = "xsFiOTLN0kUFvD2V3PoWhiU5WVLzUTXJ";
-        public const string ONEDRIVE_CLIENT_ID = "000000004C15C56E";
+        public const string DROPBOX_KEY = "DROPBOX_KEY";
+        public const string DROPBOX_SECRET = "DROPBOX_SECRET";
+        public const string ONEDRIVE_SECRET = "ONEDRIVE_SECRET";
+        public const string ONEDRIVE_CLIENT_ID = "ONEDRIVE_CLIENT_ID";
         public const string ONEDRIVE_REDIRECT = "https://login.live.com/oauth20_desktop.srf";
     }
+#endif
 }
